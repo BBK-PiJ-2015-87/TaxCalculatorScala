aa9d62779a0bc08557d8b65d49c4f437e1377012
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2016-05-10 21:34:33 +0100

    Added scalatags temlates with loop

--- src/main/scala-2.11/controllers/TagsTestController.scala
+++ src/main/scala-2.11/controllers/TagsTestController.scala
@@ -2,13 +2,23 @@ package controllers
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import views.HelloPage
+import views.{LoopView, ExampleScalaTags, HelloPage}
 
 class TagsTestController extends Controller {
-  val view = new HelloPage
+  val view1 = new HelloPage
+  val view2 = new ExampleScalaTags
+  val view3 = new LoopView
 
   get("/tags"){ request:Request =>
-    view.fragment.toString()
+    view1.fragment.toString()
+  }
+
+  get("/tagz"){ request:Request =>
+    view2.htmlFrag.toString()
+  }
+
+  get("/loop"){ request:Request =>
+    view3.fragment.toString()
   }
 
 }
